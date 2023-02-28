0e529793f6725d957eb04cb0125d35b131d219f7
Vaani-Arora <115731505+Vaani-Arora@users.noreply.github.com>
2023-02-28 20:04:54 +0530

    Update UserWidget.jsx

--- app/src/scenes/widgets/UserWidget.jsx
+++ app/src/scenes/widgets/UserWidget.jsx
@@ -24,7 +24,7 @@ import axios from "axios";
     const main = palette.neutral.main;
   
     const getUser = async () => {
-      const response = await axios.get(`https://viewb.onrender.com/users/${userId}`, {
+      const response = await axios.get(`https://view-backend.onrender.com/users/${userId}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await response.data;
