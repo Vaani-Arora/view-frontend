caa78549b6e9d123f947a093465f0cfec1e26253
Vaani-Arora <115731505+Vaani-Arora@users.noreply.github.com>
2023-02-28 20:10:40 +0530

    Update Friend.jsx

--- app/src/components/Friend.jsx
+++ app/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `https://viewb.onrender.com/users/${_id}/${friendId}`,
+      `https://view-backend.onrender.com/users/${_id}/${friendId}`,
       {
         method:"PATCH",
         headers: {
